cf72893f41d0a479e30ebfdab546b50f959eb55d
Dennis Lerch <dlerch@lerch-VirtualBox.(none)>
2014-09-16 07:33:06 +0200

    changed the filesource to mirrorservice.org, because of an ftp proxy issue

--- config/software/libffi.rb
+++ config/software/libffi.rb
@@ -21,7 +21,8 @@ default_version "3.0.13"
 dependency "libgcc"
 dependency "libtool"
 
-source :url => "ftp://sourceware.org/pub/libffi/libffi-3.0.13.tar.gz",
+#source :url => "ftp://sourceware.org/pub/libffi/libffi-3.0.13.tar.gz",
+source :url => "http://www.mirrorservice.org/sites/sourceware.org/pub/libffi/libffi-3.0.13.tar.gz",
        :md5 => '45f3b6dbc9ee7c7dfbbbc5feba571529'
 
 relative_path "libffi-3.0.13"
